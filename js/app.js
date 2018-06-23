fb50594b2bdd6d41871e20bc8c14fa70fd190ab4
Adrian Carabetta <adrian_carabetta@hotmail.com>
2018-06-23 17:49:20 +0800

    added daily/hourly option

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
+  var special = ['zeroth','first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'];
+  var deca = ['twent', 'thirt', 'fort'];
+  function stringifyNumber(n) {
+    if (n < 20) return special[n];
+    if (n%10 === 0) return deca[Math.floor(n/10)-2] + 'ieth';
+    return deca[Math.floor(n/10)-2] + 'y-' + special[n%10];
+  }
 
   // $(this).closest('.add-to-list').on('click', function()
   $(document).on('click', '.add-to-list', function(){
-  // $('.add-to-list').on('click', function() {
-  // $('#button').on('click', function() {
-    if($('.list-input').val() !== ''){
-      // console.log( $(this).prev('input[name=ListItem]').val() );
+    if( $(this).closest('.list-box').find('.list-input').val() !== ''){
+      // Grab the input from the closest input bar
       var toAdd = $(this).closest('.list-box').find('.list-input').val();
-      console.log( $(this).attr('class'));
-      $(this).siblings('.list').append('<li class="list-item">' + toAdd + '</li>');
-      // $('ol').append('<li>' + toAdd + '</li>');
-    }
-  });
 
-  $('input').focus(function() {
-    $(this).val('');
+      // Push the input to the closest list
+      $(this).siblings('.list').append(
+        '<li class="list-item">' + toAdd + '</li>'
+      );
+    }
   });
 
   //Press enter to add to list
@@ -28,23 +31,21 @@ $(document).ready(function(){
   $(document).on('click','.list-item', function(){
     // $(this).toggleClass('strike').fadeOut('slow');
     $(this).toggleClass('strike');
+    $('ol').sortable();
   });
 
   $('ol').sortable();
 
   var listMaker = function( number ){
+
     if( $('#add-hour').length) {
       $('.hour-' + number).append(
         ' <div class="list-container">' +
-          ' <h2>Hour ' + number + '</h2>' +
+          ' <h6 class="list-header">' + stringifyNumber(number) + ' hour</h6>' +
     	    '	<input type="text" name="ListItem" class="list-input" autocomplete="off"/>' +
-          // ' <button type="button" class="btn btn-secondary add-to-list">Add</button>' +
-          // ' <br/>'+
-          // ' <ol class="list"></ol> '+
         ' </div>' +
         ' <div>' +
           ' <h3 class="add-to-list">Add</h3>' +
-          // ' <br/>'+
           ' <ol class="list"></ol> '+
         ' </div>'
       );
@@ -53,7 +54,7 @@ $(document).ready(function(){
 
   if( $('#add-hour').length ){
     var numberOfRows = 0;
-    var numberOfHours = 0;
+    var numberOfHours = 1;
     $('.footer').on("click", function() {
 
       if( numberOfRows === 0 ){
@@ -61,13 +62,28 @@ $(document).ready(function(){
         numberOfRows++;
       }
 
-      if( numberOfHours < 4 ) {
+       if( numberOfHours < 25 ) {
         $('.first-row').append('<div class="col-md-3 list-box hour-' + numberOfHours + '"></div>');
         listMaker( numberOfHours );
         numberOfHours++;
-      }
+       }
 
     });
   }
 
+  $('.daily-hourly').on('click', function() {
+
+    console.log($('.daily-hourly').text());
+    if( $('.daily-hourly').text() == 'Daily'){
+      $(this).text('Hourly');
+      $('.task-holder').hide();
+      $('.daily-container').show();
+    }
+    else if( $('.daily-hourly').text() == 'Hourly'){
+      $(this).text('Daily');
+      $('.task-holder').show();
+      $('.daily-container').hide();
+    }
+
+  });
 });
